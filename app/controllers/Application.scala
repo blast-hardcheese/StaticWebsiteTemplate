84161892dc076b44b868f45f5baaacb395691be9
Devon Stewart <blast@hardchee.se>
2013-10-05 13:42:38 +0900

    Transitioning index over to being multilingual

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,8 +6,18 @@ import play.api.mvc._
 object Application extends Controller {
   def buildLang(lang: String) = play.api.i18n.Lang(lang.toLowerCase)
 
+  def doIndex(langStr: String) = {
+    implicit val lang = buildLang(langStr)
+    implicit val currentPage: Function1[String, Call] = routes.Application.indexLang _
+    views.html.index()
+  }
+
   def index = Action {
-    Ok(views.html.index())
+    Ok(doIndex("en-us"))
+  }
+
+  def indexLang(langStr: String) = Action {
+    Ok(doIndex(langStr))
   }
 
   def doTemplate(langStr: String) = {
