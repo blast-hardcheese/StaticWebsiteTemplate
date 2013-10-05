b67eda90c64053a43544667d1eda0c182b031bbf
Devon Stewart <blast@hardchee.se>
2013-10-05 13:34:52 +0900

    Adding a buildLang helper method

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,6 +4,7 @@ import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
+  def buildLang(lang: String) = play.api.i18n.Lang(lang.toLowerCase)
 
   def index = Action {
     Ok(views.html.index())
